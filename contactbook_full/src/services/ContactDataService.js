72b4fdbf62b5c65ce1dabef2f7314ff518618525
thao <littlelili0994@gmail.com>
2023-04-17 15:37:34 +0700

    first commit

---
+++ contactbook_full/src/services/ContactDataService.js
@@ -0,0 +1,36 @@
+import http from "../3-4 Frontend/contactbook_frontend_v2/src/http-common";
+
+class ContactDataService {
+    getAll() {
+        return http.get("/contacts");
+    }
+
+    get(id) {
+        return http.get(`/contacts/${id}`);
+    }
+
+    create(data) {
+        return http.post("/contacts", data);
+    }
+
+
+
+    update(id, data) {
+
+        return http.put(`/contacts/${id}`, data);
+    }
+
+    delete(id) {
+        return http.delete(`/contacts/${id}`);
+    }
+
+    deleteAll() {
+        return http.delete(`/contacts`);
+    }
+
+    findByTitle(title) {
+        return http.get(`/contacts?title=${title}`);
+    }
+}
+
+export default new ContactDataService();
